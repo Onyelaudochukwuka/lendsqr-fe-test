4e2afa69a33cf2f677e1cc12255e5014f850a550
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 12:12:08 +0100

    added error state

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { loginIllustration, Logo } from "../../assets";
 import { Input, PassWordInput } from "../../components";
 import { useInput } from "../../utils/hooks";
@@ -7,6 +7,8 @@ interface ILogIn {}
 const LogIn: FC<ILogIn> = () => {
   const [email, setEmail, clearEmail] = useInput("");
   const [password, setPassword, clearPassword] = useInput("");
+  const [emailError, setEmailError] = useState(true);
+  const [passWordError, setPassWordError] = useState(true);
   return (
     <section className={style.LogIn}>
       <div className={style.LogIn__left}>
@@ -19,24 +21,32 @@ const LogIn: FC<ILogIn> = () => {
       <div className={style.LogIn__right}>
         <h2 className={style.LogIn__right__heading}>Welcome!</h2>
         <p className={style.LogIn__right__details}>Enter details to login.</p>
-        <Input
-          {...{
-            value: email,
-            setValue: setEmail,
-            clearValue: clearEmail,
-            placeholder: "Email",
-            type: "email",
-          }}
-        />
-        <PassWordInput
-          {...{
-            value: password,
-            setValue: setPassword,
-            clearValue: clearPassword,
-            placeholder: "Password",
-            type: "password",
-          }}
-        />
+        <div className={style.LogIn__right__form}>
+          <Input
+            {...{
+              value: email,
+              setValue: setEmail,
+              clearValue: clearEmail,
+              placeholder: "Email",
+              type: "email",
+              error: emailError,
+              errorMessage: "Please enter a valid email",
+            }}
+          />
+          <PassWordInput
+            {...{
+              value: password,
+              setValue: setPassword,
+              clearValue: clearPassword,
+              placeholder: "Password",
+              type: "password",
+            }}
+          />
+          <span className={style.LogIn__right__form__forgot}>
+            Forgot PASSWORD?
+          </span>
+          <button className={style.LogIn__right__form__button}>Login</button>
+        </div>
       </div>
     </section>
   );
