85b4e6b6297ca1995873f143d51d401e418af190
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 09:34:19 +0100

    created input component for text inputs

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react"
+
+interface IInput {
+
+}
+const Input: FC<IInput> = () => {
+    return (
+        <div>
+            Input
+        </div>
+
+    );
+}
+
+export default Input;
