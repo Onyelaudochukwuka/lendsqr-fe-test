a399ab8a11942727f856b0b7776b8a344871666c
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 09:36:27 +0100

    created stylesheets for login page

---
+++ src/pages/LogIn/LogIn.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react"
+
+interface ILogIn {
+
+}
+const LogIn: FC<ILogIn> = () => {
+    return (
+        <div>
+            LogIn
+        </div>
+
+    );
+}
+
+export default LogIn;
