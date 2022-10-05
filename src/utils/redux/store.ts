bcd193ada1fd0cc83884ead3967f2970ee2406f7
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 20:06:21 +0100

    indentation fixes

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import apiConnection from "./apiConnection";
 import AppSlice from "./appSlice";
+import apiConnection from "./apiConnection";
 
 const store = configureStore({
-    reducer: {
-        [AppSlice.name]: AppSlice.reducer,
-        [apiConnection.reducerPath]: apiConnection.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiConnection.middleware),
+  reducer: {
+    [AppSlice.name]: AppSlice.reducer,
+    [apiConnection.reducerPath]: apiConnection.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiConnection.middleware)
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
+
