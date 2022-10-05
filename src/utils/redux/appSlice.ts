b731c80cd4b8e46bd28cb09fabf35e5d596a1b3b
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 20:06:07 +0100

    indentation fixes

--- src/utils/redux/appSlice.ts
+++ src/utils/redux/appSlice.ts
@@ -1,23 +1,24 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface AppState {
-    loggedIn: boolean
+  loggedIn: boolean
 }
 const initialState: AppState = {
-    loggedIn: false
-}
+  loggedIn: false
+};
 const AppSlice = createSlice({
-    name: "app",
-    initialState,
-    reducers: {
-        logIn(state: AppState, action: PayloadAction<AppState>) {
-            // A demo since we won't actually be managing such states
-            state.loggedIn = true;
-        },
-        logOut(state: AppState, action: PayloadAction<AppState>) {
-            // A demo since we won't actually be managing such states
-            state.loggedIn = false;
-        }
-    }
-})
-export const { logIn, logOut } = AppSlice.actions;
+  name: "app",
+  initialState,
+  reducers: {
+    logIn (state: AppState, action: PayloadAction<AppState>) {
+      // A demo since we won't actually be managing such states
+      state.loggedIn = true
+    },
+    logOut (state: AppState, action: PayloadAction<AppState>) {
+      // A demo since we won't actually be managing such states
+      state.loggedIn = false
+    },
+  },
+});
+export const { logIn, logOut } = AppSlice.actions
+
 export default AppSlice;
