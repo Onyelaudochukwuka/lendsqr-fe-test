7c2434f59574a9ace59993e5e3f2871f29503730
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:52:19 +0100

    adding on markup

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,33 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import { Logo } from "../../assets";
+import { useInput } from "../../utils/hooks";
+import Input from "../Input/Input";
 import style from "./index.module.css";
 interface INavbar {
- className: string;
+  className: string;
 }
-const Navbar: FC<INavbar> = ({className}) => {
-    return (
-        <div className={`${style.Navbar} ${className}`}>
-            Navbar
-        </div>
-
-    );
-}
+const Navbar: FC<INavbar> = ({ className }) => {
+  const [search, changeSearch, clearSearch] = useInput("");
+  return (
+    <div className={`${style.Navbar} ${className}`}>
+      <Logo className={style.Navbar__logo} />
+          <Input
+            className={style.Navbar__search}
+        {...{
+          placeholder: "Search",
+          type: "text",
+          value: search,
+          setValue: changeSearch,
+          clearValue: clearSearch,
+        }}
+          />
+          <div className={style.Navbar__info}>
+              <Link to="/Dashboard" className={style.Navbar__info__docs}><span>Docs</span></Link>
+              
+          </div>
+    </div>
+  );
+};
 
 export default Navbar;
