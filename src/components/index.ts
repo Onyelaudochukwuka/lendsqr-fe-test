12cf342e7fbd9f4c188ebc406ee055a3a257943a
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:26:31 +0100

    exported sidebar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
-export { default as Input } from "./Input/Input"; 
+export { default as Input } from "./Input/Input";
 export { default as PassWordInput } from "./PassWordInput/PassWordInput";
 export { default as Loader } from "./Loader/Loader";
+export { default as Layout } from "./Layout/Layout";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Sidebar } from "./Sidebar/Sidebar";
