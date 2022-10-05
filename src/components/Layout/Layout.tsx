651b02f8aa4b318920b555a9581026c89e3446a7
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:32:47 +0100

    added classNames

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
 import React, { FC, ReactNode } from "react";
-import { Navbar } from "../";
+import { Navbar, Sidebar } from "../";
 import style from "./index.module.css";
 interface ILayout {
-    children: ReactNode;
-    className: string;
+  children: ReactNode;
+  className: string;
 }
-const Layout: FC<ILayout> = ({ children,className,  ...props }) => {
+const Layout: FC<ILayout> = ({ children, className, ...props }) => {
   return (
     <section className={style.Layout}>
       <Navbar className={style.Layout__nav} />
-      <section {...props}>{children}</section>
+      <Sidebar className={style.Layout__sidebar} />
+      <section {...props} className={`${className} ${style.Layout__body}`}>{children}</section>
     </section>
   );
 };
