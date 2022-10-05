f27c00ed9c092910ed72e1ab2e69f2e330a1c74f
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 20:05:49 +0100

    added logic for error popUp

--- src/components/PassWordInput/PassWordInput.tsx
+++ src/components/PassWordInput/PassWordInput.tsx
@@ -1,25 +1,31 @@
 import React, { ChangeEvent, FC, useRef } from "react";
 import style from "./index.module.css";
 interface IPassWordInput {
-  placeholder: string;
-  value: string;
-  setValue: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
-  type: string;
-  clearValue: () => void;
+  placeholder: string
+  value: string
+  setValue: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
+  type: string
+  clearValue: () => void
+  error: boolean
+  errorMessage: string
 }
+
 const PassWordInput: FC<IPassWordInput> = ({
   placeholder,
   value,
   setValue,
   type,
   clearValue,
+  error,
+  errorMessage
 }) => {
   const inputEl = useRef<HTMLInputElement>(null);
   return (
     <div className={style.PassWordInput}>
+    <div className={style.PassWordInput__container}>
       <input
         ref={inputEl}
-        className={style.PassWordInput__input}
+          className={style.PassWordInput__container__input}
         {...{ placeholder, value, type }}
         onChange={setValue}
       />
@@ -30,10 +36,12 @@ const PassWordInput: FC<IPassWordInput> = ({
               inputEl.current.type === type ? "text" : "password";
           }
               }}
-              className={style.PassWordInput__show}
+          className={style.PassWordInput__container__show}
       >
         show
       </span>
+    </div>
+      {error && <p className={style.PassWordInput__error}>{errorMessage}</p>}
     </div>
   );
 };
