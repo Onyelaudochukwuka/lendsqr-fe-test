e9993491348b7a05c61c0e2dd17c10d26b0f0973
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:32:40 +0100

    added layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from "react"
+import { Layout } from "../../components";
 import style from "./index.module.css";
 interface IDashboard {
 
 }
 const Dashboard: FC<IDashboard> = () => {
     return (
-        <div className={style.Dashboard}>
-            Dashboard
-        </div>
+        <Layout className={style.Dashboard}>
+                Dashboard
+        </Layout>
 
     );
 }
