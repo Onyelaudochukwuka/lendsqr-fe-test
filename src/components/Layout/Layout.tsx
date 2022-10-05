882a4d549913f3c3acebb11886e22aa0ca9396bc
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:27:08 +0100

    created layout wrapper

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,17 @@
-import React, { FC } from "react"
+import React, { FC, ReactNode } from "react";
+import { Navbar } from "../";
 import style from "./index.module.css";
 interface ILayout {
-
+    children: ReactNode;
+    className: string;
 }
-const Layout: FC<ILayout> = () => {
-    return (
-        <div className={style.Layout}>
-            Layout
-        </div>
-
-    );
-}
+const Layout: FC<ILayout> = ({ children,className,  ...props }) => {
+  return (
+    <section className={style.Layout}>
+      <Navbar className={style.Layout__nav} />
+      <section {...props}>{children}</section>
+    </section>
+  );
+};
 
 export default Layout;
