b39d404c7c4258602155ff362c1e1bdbe555d8ab
Udoka <udochukwukaonyela@gmail.com>
2022-10-05 21:08:53 +0100

    removed logo import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { FC, Fragment, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { Login } from "./pages";
