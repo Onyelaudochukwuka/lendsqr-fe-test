7bccb69bb68c523afd28015884f0f0d47889d9ef
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 00:25:52 +0100

    added logc for navigation

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useEffect, useState } from "react";
 import { Filter } from "../../assets";
 import style from "./index.module.css";
-import moment from "moment";
-import { TableRow } from "../";
 import { useGetUsersQuery } from "../../utils/redux/apiConnection";
+import { SelectViews, TableRow } from "../";
 interface ITable {}
 const heading: string[] = [
   "Organization",
@@ -11,31 +10,42 @@ const heading: string[] = [
   "Email",
   "Phone Number",
   "Date Joined",
-  "Status"
+  "Status",
 ];
 const Table: FC<ITable> = () => {
   const { data: rows, isLoading } = useGetUsersQuery({});
+  const [select, setSelect] = useState<number>(100);
+  const [data, setData] = useState(rows);
+  useEffect(() => {
+    if (rows) {
+      setData(rows.slice(0, select));
+    }
+  }, [rows, select]);
   return (
-    <div className={style.Table}>
-      <div className={style.Table__header}>
-        {heading.map((head) => (
-          <div className={style.Table__header__heading}>
-            <span className={style.Table__header__heading__content}>
-              {head}
-            </span>
-            <Filter className={style.Table__header__heading__icon} />
-          </div>
-        ))}
-      </div>
-      {/* <div className={style.Table__row}>
-        {!isLoading && rows?.map((data: any) => <TableRow {...data} />)}
-      </div> */}
-      <div className={style.Table__footer}>
-        <div className={style.Table__footer_select}>
-          <span>Showing  entries</span>
+    <>
+      <div className={style.Table}>
+        <div className={style.Table__header}>
+          {heading.map((head) => (
+            <div className={style.Table__header__heading}>
+              <span className={style.Table__header__heading__content}>
+                {head}
+              </span>
+              <Filter className={style.Table__header__heading__icon} />
+            </div>
+          ))}
         </div>
-    </div>
-    </div>
+        <div className={style.Table__row}>
+        {!isLoading && data?.map((data: any) => <TableRow {...data} />)}
+      </div>
+      </div>
+      <div className={style.Table__footer}>
+        <div className={style.Table__footer__select}>
+          <span>Showing </span>
+          <SelectViews {...{ select, setSelect }} /> <span>out of 1000</span>
+        </div>
+        
+      </div>
+    </>
   );
 };
 
