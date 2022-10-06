10d50638b51dc632a1a2a68e392a9b43f8410a69
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 14:18:21 +0100

    added userStats card

--- src/pages/UserDashboard/UserDashboard.tsx
+++ src/pages/UserDashboard/UserDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react"
-import { Layout } from "../../components";
+import { Layout, UsersStats } from "../../components";
 import style from "./index.module.css";
 import { useGetUsersQuery } from "../../utils/redux/apiConnection";
 interface IUserDashboard {
@@ -9,8 +9,8 @@ const UserDashboard: FC<IUserDashboard> = () => {
     return (
         <Layout className={style.UserDashboard}>
             <h2 className={style.UserDashboard__heading}>Users</h2>
-            <div className={style.UserDashboard__cards}>
-                
+            <div className={style.UserDashboard__stats}>
+                <UsersStats />
             </div>
         </Layout>
 
