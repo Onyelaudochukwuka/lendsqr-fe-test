44d1e4a24e60d3dc913e220cee42655e49d83a97
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 14:10:59 +0100

    created container for cards

--- src/pages/UserDashboard/UserDashboard.tsx
+++ src/pages/UserDashboard/UserDashboard.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from "react"
 import { Layout } from "../../components";
 import style from "./index.module.css";
+import { useGetUsersQuery } from "../../utils/redux/apiConnection";
 interface IUserDashboard {
 
 }
 const UserDashboard: FC<IUserDashboard> = () => {
     return (
         <Layout className={style.UserDashboard}>
-                Dashboard
+            <h2 className={style.UserDashboard__heading}>Users</h2>
+            <div className={style.UserDashboard__cards}>
+                
+            </div>
         </Layout>
 
     );
