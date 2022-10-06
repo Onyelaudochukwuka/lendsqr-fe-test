0fb2cb04e20949cce6640cfcd1f6639f353f5520
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 20:12:20 +0100

    created table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,15 +1,55 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
+import { Filter } from "../../assets";
 import style from "./index.module.css";
-interface ITable {
-
-}
+import moment from "moment";
+import { TableRow } from "../";
+interface ITable {}
+const heading: string[] = [
+  "Organization",
+  "Name",
+  "Email",
+  "Phone Number",
+  "Date Joined",
+  "Status",
+];
+const row = [
+  {
+    orgName: "Lendsqr",
+    userName: "Adedeji",
+    email: "adedeji@lendsqr.com",
+    phoneNumber: "08078903721",
+    createdAt: "May 15, 2020 10:00 AM",
+    status: "Inactive",
+  },{
+    orgName: "Lendsqr",
+    userName: "Adedeji",
+    email: "adedeji@lendsqr.com",
+    phoneNumber: "08078903721",
+    createdAt: "May 15, 2020 10:00 AM",
+    status: "Inactive",
+  }
+];
 const Table: FC<ITable> = () => {
-    return (
-        <div className={style.Table}>
-            Table
-        </div>
-
-    );
-}
+  console.log(
+    moment("2072-12-27T03:44:22.522Z").format("MMMM D YYYY, h:mm:ss A")
+  );
+  return (
+    <div className={style.Table}>
+      <div className={style.Table__header}>
+        {heading.map((head) => (
+          <div className={style.Table__header__heading}>
+            <span className={style.Table__header__heading__content}>
+              {head}
+            </span>
+            <Filter className={style.Table__header__heading__icon} />
+          </div>
+        ))}
+      </div>
+      <div className={style.Table__row}>
+        {row.map((data) => (<TableRow {...data} />))}
+      </div>
+    </div>
+  );
+};
 
 export default Table;
