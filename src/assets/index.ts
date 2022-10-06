e0fa077ed52063b7ae6a9dd027fbef60be70c26f
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 09:54:19 +0100

    exported assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ import userImage from "./user-image.png";
 export { default as Logo } from "./icons/Logo";
 export { default as Notification } from "./icons/Notification";
 export { default as Search } from "./icons/Search";
-export {default as DropDown} from "./icons/DropDown";
+export { default as DropDown } from "./icons/DropDown";
+export { default as BriefCase } from "./icons/BriefCase";
 export { loginIllustration, userImage };
