dbc2a2b89fc3fb5eb26cf56fa65a6213cb383f1c
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 14:46:08 +0100

    added texts

--- src/components/UsersStats/UsersStats.tsx
+++ src/components/UsersStats/UsersStats.tsx
@@ -1,19 +1,15 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
 import { DashboardUsers } from "../../assets";
 import style from "./index.module.css";
-interface IUsersStats {
-
-}
+interface IUsersStats {}
 const UsersStats: FC<IUsersStats> = () => {
-    return (
-        <div className={style.UsersStats}>
-            <DashboardUsers className={style.UsersStats__icon} />
-            
-            
-            
-        </div>
-
-    );
-}
+  return (
+    <div className={style.UsersStats}>
+      <DashboardUsers className={style.UsersStats__icon} />
+          <span className={style.UsersStats__heading}>USERS</span>
+          <span className={style.UsersStats__count}>2,453</span>
+    </div>
+  );
+};
 
 export default UsersStats;
