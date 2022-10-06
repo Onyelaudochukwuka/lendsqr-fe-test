95b12f13059a26b762d47f380d6b88c78c2fab0f
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 11:54:19 +0100

    added elements and styles

--- src/components/SidebarSection/SidebarSection.tsx
+++ src/components/SidebarSection/SidebarSection.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react"
+import { SidebarElements } from "../Sidebar/Sidebar";
+import SideBarLink from "../SideBarLink/SideBarLink";
 import style from "./index.module.css";
 interface ISidebarSection {
 
 }
-const SidebarSection: FC<ISidebarSection> = () => {
+const SidebarSection: FC<ISidebarSection & SidebarElements> = ({heading, children}) => {
     return (
         <div className={style.SidebarSection}>
-            SidebarSection
+            <p className={style.SidebarSection__heading}>{heading}</p>
+            {children.map(({ Icon, name, to }) => (
+                <SideBarLink to={to}>
+                    <Icon className={style.SidebarSection__icon} />
+                    <span>{name}</span>
+                </SideBarLink>
+            ))}
         </div>
 
     );
