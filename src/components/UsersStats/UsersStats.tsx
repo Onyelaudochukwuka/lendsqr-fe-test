fd636851cf7523af789c71c0d31a2e30b0bad77a
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 15:33:23 +0100

    added logic for mapping through data

--- src/components/UsersStats/UsersStats.tsx
+++ src/components/UsersStats/UsersStats.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from "react";
 import { DashboardUsers } from "../../assets";
 import style from "./index.module.css";
-interface IUsersStats {}
-const UsersStats: FC<IUsersStats> = () => {
+interface IUsersStats {
+  Icon: FC<{ className: string }>;
+  heading: string;
+  count: number;
+}
+const UsersStats: FC<IUsersStats> = ({ Icon, heading, count }) => {
   return (
     <div className={style.UsersStats}>
-      <DashboardUsers className={style.UsersStats__icon} />
-          <span className={style.UsersStats__heading}>USERS</span>
-          <span className={style.UsersStats__count}>2,453</span>
+      <Icon className={style.UsersStats__icon} />
+          <span className={style.UsersStats__heading}>{heading}</span>
+      <span className={style.UsersStats__count}>{count.toLocaleString()}</span>
     </div>
   );
 };
