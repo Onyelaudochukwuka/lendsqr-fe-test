30303de7911059b7cb98482183a9fb6b5cd6781b
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 10:53:35 +0100

    created sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,24 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
 import { SideBarLink } from "../";
-import { BriefCase } from "../../assets";
+import { BriefCase, Home, OrgDropDown } from "../../assets";
 import style from "./index.module.css";
 interface ISidebar {
-    className: string;
+  className: string;
 }
-const Sidebar: FC<ISidebar> = ({className}) => {
-    return (
-        <div className={`${style.Sidebar} ${className}`}>
-            <div className={style.Sidebar__switch}><BriefCase className={style.Sidebar__switch_icon} />  <span>Switch Organization</span>  </div>
-        </div>
-
-    );
-}
+const Sidebar: FC<ISidebar> = ({ className }) => {
+  return (
+    <div className={`${style.Sidebar} ${className}`}>
+      <div className={style.Sidebar__switch}>
+        <BriefCase className={style.Sidebar__switch_icon} />{" "}
+        <span>Switch Organization</span>{" "}
+        <OrgDropDown className={style.Sidebar__switch__drop} />{" "}
+      </div>
+      <SideBarLink to="/dashboard">
+              <Home className={style.Sidebar__link__icon} />
+              <span>Dashboard</span>
+      </SideBarLink>
+    </div>
+  );
+};
 
 export default Sidebar;
