6ad9cc4d3adb42bbc9446191ca78002228a59018
Udoka <udochukwukaonyela@gmail.com>
2022-10-06 06:10:49 +0100

    created navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
-import { Logo, Notification } from "../../assets";
+import { DropDown, Logo, Notification, Search, userImage } from "../../assets";
 import { useInput } from "../../utils/hooks";
 import Input from "../Input/Input";
 import style from "./index.module.css";
@@ -11,21 +11,39 @@ const Navbar: FC<INavbar> = ({ className }) => {
   const [search, changeSearch, clearSearch] = useInput("");
   return (
     <div className={`${style.Navbar} ${className}`}>
-      <Logo className={style.Navbar__logo} />
-          <Input
-            className={style.Navbar__search}
-        {...{
-          placeholder: "Search",
-          type: "text",
-          value: search,
-          setValue: changeSearch,
-          clearValue: clearSearch,
-        }}
+      <div className={style.Navbar__left}>
+        <Logo className={style.Navbar__left__logo} />
+        <div className={style.Navbar__left__search}>
+        <Input
+          className={style.Navbar__left__search__input}
+          {...{
+            placeholder: "Search",
+            type: "text",
+            value: search,
+            setValue: changeSearch,
+            clearValue: clearSearch,
+          }}
           />
-          <div className={style.Navbar__info}>
-              <Link to="/Dashboard" className={style.Navbar__info__docs}><span>Docs</span></Link>
-              <Notification className={style.Navbar__info__notification} />
+          <div className={style.Navbar__left__search__searchbar}><Search className={style.Navbar__left__search__searchbar__icon} /></div>
           </div>
+      </div>
+      <div className={style.Navbar__right}>
+        <Link to="/Dashboard" className={style.Navbar__right__docs}>
+          <span>Docs</span>
+        </Link>
+        <Notification className={style.Navbar__right__notification} />
+        <div className={style.Navbar__right__user}>
+          <img
+            src={userImage}
+            alt="userImage"
+            className={style.Navbar__right__user__avatar}
+          />
+          <span className={style.Navbar__right__user__info}>
+            <p className={style.Navbar__right__user__info__name}>Adedeji</p>
+            <DropDown className={style.Navbar__right__user__info__icon} />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
