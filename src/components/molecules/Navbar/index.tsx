aba92368d3014e667bb1bb319c1b0e0dc3ddb42f
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 22:23:29 +0100

    formatted

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
-import { DropDown, Logo, Notification, Search, userImage } from "../../../assets";
+import {
+  DropDown,
+  Logo,
+  Notification,
+  Search,
+  userImage,
+} from "../../../assets";
 import { useInput } from "../../../utils/hooks";
 import Input from "../Input";
 import style from "./index.module.css";
@@ -8,24 +14,26 @@ interface INavbar {
   className: string;
 }
 const Navbar: FC<INavbar> = ({ className }) => {
-  const [search, changeSearch, clearSearch] = useInput("");
+  const [search, changeSearch, clearSearch] = useInput<string>("");
   return (
     <div className={`${style.Navbar} ${className}`}>
       <div className={style.Navbar__left}>
         <Logo className={style.Navbar__left__logo} />
         <div className={style.Navbar__left__search}>
-        <Input
-          className={style.Navbar__left__search__input}
-          {...{
-            placeholder: "Search",
-            type: "text",
-            value: search,
-            setValue: changeSearch,
-            clearValue: clearSearch,
-          }}
+          <Input
+            className={style.Navbar__left__search__input}
+            {...{
+              placeholder: "Search",
+              type: "text",
+              value: String(search),
+              setValue: changeSearch,
+              clearValue: clearSearch,
+            }}
           />
-          <div className={style.Navbar__left__search__searchbar}><Search className={style.Navbar__left__search__searchbar__icon} /></div>
+          <div className={style.Navbar__left__search__searchbar}>
+            <Search className={style.Navbar__left__search__searchbar__icon} />
           </div>
+        </div>
       </div>
       <div className={style.Navbar__right}>
         <Link to="/Dashboard" className={style.Navbar__right__docs}>
