f9b9fc8262684f1270aebaf3463ea24d9787a9d5
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 22:46:38 +0100

    created select date component

---
+++ src/components/atoms/SelectDate/index.tsx
@@ -0,0 +1,40 @@
+import React, { ChangeEvent, FC, useRef } from "react";
+import { DateIcon } from "../../../assets";
+import style from "./index.module.css";
+interface ISelectDate {
+  placeholder: string;
+  value: string | number;
+  setValue: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  type: string;
+  clearValue: () => void;
+  className?: string;
+  label?: string;
+}
+const SelectDate: FC<ISelectDate> = ({
+  placeholder,
+  value,
+  setValue,
+  type,
+  clearValue,
+  className,
+  label,
+}) => {
+  const inputEl = useRef<HTMLInputElement>(null);
+  return (
+    <div className={`${style.SelectDate} ${className}`}>
+      {!!label && <div className={style.SelectDate__label}>{label}</div>}
+      <div className={style.SelectDate__container}>
+        <input
+          ref={inputEl}
+          className={`${style.SelectDate__container__input} ${className}`}
+          {...{ placeholder, value, type: value === "" ? "text" : type }}
+                  onChange={setValue}
+                  onFocus={() => inputEl.current?.setAttribute("type", "date")}
+        />
+              { value === "" && <DateIcon className={style.SelectDate__container__icon} />}
+      </div>
+    </div>
+  );
+};
+
+export default SelectDate;
