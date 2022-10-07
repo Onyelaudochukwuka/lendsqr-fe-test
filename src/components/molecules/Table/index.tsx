bc16110356411d0d60935321fc1f83bb9c9de3a2
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 14:53:12 +0100

    added logic for footer nav controller

--- src/components/molecules/Table/index.tsx
+++ src/components/molecules/Table/index.tsx
@@ -17,13 +17,40 @@ const Table: FC<ITable> = () => {
   const { data: rows, isLoading } = useGetUsersQuery({});
   const [select, setSelect] = useState<number>(10);
   const [data, setData] = useState(rows);
-  const limit = !!rows ? rows?.length / select : 0;
-  console.log(limit);
+
+  const limit = !!rows ? Math.round(rows?.length / select) : 0;
+  const indexArr = [...Array(limit + 1).keys()].splice(1);
+  const [currentIndex, setCurrentIndex] = useState<number>(1);
+  const currentData = [
+    indexArr.slice(
+      currentIndex + 3 > indexArr.length
+        ? indexArr.length - 4
+        : currentIndex - 1,
+      currentIndex + 3 > indexArr.length
+        ? indexArr.length - 2
+        : currentIndex + 1
+    ),
+    indexArr.slice(indexArr.length - 2),
+  ];
+  const value = currentData.map((items) =>
+    items.map((item) => ({ value: item, active: item === currentIndex }))
+  );
+  console.log(value, limit);
   useEffect(() => {
     if (rows) {
-      setData(rows.slice(0, select));
+      setData(
+        rows.filter(
+          (_: unknown, index: number) =>
+            index >= select * currentIndex - select &&
+            index < select * currentIndex
+        )
+      );
     }
-  }, [rows, select]);
+  }, [rows, select, currentIndex]);
+  useEffect(() => {
+    setCurrentIndex(1);
+  }, [select]);
+
   return (
     <>
       <div className={style.Table}>
@@ -44,19 +71,59 @@ const Table: FC<ITable> = () => {
       <div className={style.Table__footer}>
         <div className={style.Table__footer__select}>
           <span>Showing </span>
-          <SelectViews {...{ select, setSelect }} /> <span>out of 1000</span>
+          <SelectViews {...{ select, setSelect }} />
+          <span>out of 1000</span>
         </div>
         <div className={style.Table__footer__navigation}>
-          <LeftNavigation className={style.Table__footer__navigation__icon__left} />
-          <div>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-
+          <div
+            className={style.Table__footer__navigation__icon}
+            onClick={() =>
+              setCurrentIndex((prev) => (prev - 1 > 0 ? prev - 1 : prev))
+            }
+          >
+            <LeftNavigation
+              className={style.Table__footer__navigation__icon__left}
+            />
           </div>
-          <RightNavigation className={style.Table__footer__navigation__icon__right} />
+          <div className={style.Table__footer__navigation__details}>
+            {value[0].map(({ value, active }) => (
+              <div
+                key={`firstIndex-${value}`}
+                onClick={() => setCurrentIndex(value)}
+                className={`${style.Table__footer__navigation__details__unit} ${
+                  active &&
+                  style.Table__footer__navigation__details__unit__active
+                }`}
+              >
+                {value}
+              </div>
+            ))}
+            <span className={style.Table__footer__navigation__details__unit}>
+              ...
+            </span>
+            {value[1].map(({ value, active }) => (
+              <div
+                key={`firstIndex-${value}`}
+                onClick={() => setCurrentIndex(value)}
+                className={`${style.Table__footer__navigation__details__unit} ${
+                  active &&
+                  style.Table__footer__navigation__details__unit__active
+                }`}
+              >
+                {value}
+              </div>
+            ))}
+          </div>
+          <div
+            className={style.Table__footer__navigation__icon}
+            onClick={() =>
+              setCurrentIndex((prev) => (prev + 1 > limit ? prev : prev + 1))
+            }
+          >
+            <RightNavigation
+              className={style.Table__footer__navigation__icon__right}
+            />
+          </div>
         </div>
       </div>
     </>
