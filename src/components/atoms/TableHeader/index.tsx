076b52d409617d9c828ec01be0ae8ccb488f1a99
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 19:43:34 +0100

    added filter toggle

--- src/components/atoms/TableHeader/index.tsx
+++ src/components/atoms/TableHeader/index.tsx
@@ -1,49 +1,81 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react";
 import { Filter } from "../../../assets";
+import { SelectInput } from "../../";
 import style from "./index.module.css";
-interface ITableHeader { };
+interface ITableHeader {
+  orgNames: string[];
+}
 interface Heading {
-    value: string;
-    mobile?: boolean;
+  value: string;
+  mobile?: boolean;
 }
 const heading: Required<Heading[]> = [
-    {
-        value: "Organization",
-    },
-    {
-        value: "Username",
-        mobile: true,
-    },
-    {
-        value: "Email",
-    },
+  {
+    value: "Organization",
+  },
+  {
+    value: "Username",
+    mobile: true,
+  },
+  {
+    value: "Email",
+  },
 
-    {
-        value: "Phone Number",
-    },
-    {
-        value: "Date Joined",
-        mobile: true,
-    },
-    {
-        value: "Status",
-        mobile: true,
-    },
+  {
+    value: "Phone Number",
+  },
+  {
+    value: "Date Joined",
+    mobile: true,
+  },
+  {
+    value: "Status",
+    mobile: true,
+  },
 ];
 
-const TableHeader: FC<ITableHeader> = (props) => {
-    return (
-        <div className={style.TableHeader}>
-            {heading.map((prop) => (
-                <div className={`${style.TableHeader__heading}  ${!!prop.mobile && style.TableHeader__heading__display}`}>
-                    <span className={style.TableHeader__heading__content}>
-                        {prop.value}
-                    </span>
-                    <Filter className={style.TableHeader__heading__icon} />
-                </div>
-            ))}
+const TableHeader: FC<ITableHeader> = ({ orgNames, ...props }) => {
+  const [toggleFilter, setToggleFilter] = useState<boolean>(false);
+  const [selectedOrg, setSelectedOrg] = useState<string[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
+  return (
+    <div className={style.TableHeader}>
+      <div className={style.TableHeader__heading}>
+        {heading.map((prop) => (
+          <div
+            className={`${style.TableHeader__heading__container}  ${
+              !!prop.mobile && style.TableHeader__heading__container__display
+            }`}
+          >
+            <span className={style.TableHeader__heading__container__content}>
+              {prop.value}
+            </span>
+            <Filter
+              className={style.TableHeader__heading__container__icon}
+              onClick={() => setToggleFilter((props) => !props)}
+            />
+          </div>
+        ))}
+      </div>
+      {true && (
+        <div className={style.TableHeader__filter}>
+          <div className={style.TableHeader__filter__container}>
+            <div className={style.TableHeader__filter__container__input}>
+              <SelectInput
+                {...{
+                  selectedData: selectedOrg,
+                  setSelectedData: setSelectedOrg,
+                  data: [""],
+                  label: "Organization",
+                  placeholder: "Select",
+                }}
+              />
+            </div>
+          </div>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default TableHeader;
