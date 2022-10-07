27fd3745e58f64cdc97696b7e69eec4a0e05ae42
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 11:51:16 +0100

    adding logic for navigation

--- src/components/molecules/Table/index.tsx
+++ src/components/molecules/Table/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { Filter } from "../../../assets";
+import { Filter, LeftNavigation, RightNavigation } from "../../../assets";
 import style from "./index.module.css";
 import { useGetUsersQuery } from "../../../utils/redux/apiConnection";
 import { SelectViews, TableRow } from "../..";
@@ -12,10 +12,13 @@ const heading: string[] = [
   "Date Joined",
   "Status",
 ];
+
 const Table: FC<ITable> = () => {
   const { data: rows, isLoading } = useGetUsersQuery({});
-  const [select, setSelect] = useState<number>(100);
+  const [select, setSelect] = useState<number>(10);
   const [data, setData] = useState(rows);
+  const limit = !!rows ? rows?.length / select : 0;
+  console.log(limit);
   useEffect(() => {
     if (rows) {
       setData(rows.slice(0, select));
@@ -35,15 +38,26 @@ const Table: FC<ITable> = () => {
           ))}
         </div>
         <div className={style.Table__row}>
-        {!isLoading && data?.map((data: any) => <TableRow {...data} />)}
-      </div>
+          {!isLoading && data?.map((data: any) => <TableRow {...data} />)}
+        </div>
       </div>
       <div className={style.Table__footer}>
         <div className={style.Table__footer__select}>
           <span>Showing </span>
           <SelectViews {...{ select, setSelect }} /> <span>out of 1000</span>
         </div>
-        
+        <div className={style.Table__footer__navigation}>
+          <LeftNavigation className={style.Table__footer__navigation__icon__left} />
+          <div>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+
+          </div>
+          <RightNavigation className={style.Table__footer__navigation__icon__right} />
+        </div>
       </div>
     </>
   );
