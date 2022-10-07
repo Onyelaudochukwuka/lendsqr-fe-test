45b97087cf7f408194a94e49ca044f492f3f4409
Udoka <udochukwukaonyela@gmail.com>
2022-10-07 22:22:48 +0100

    expored iconsd

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -36,5 +36,5 @@ export { default as Activate } from "./icons/Activate";
 export { default as Blacklist } from "./icons/Blacklist";
 export { default as LeftNavigation } from "./icons/LeftNavigation";
 export { default as RightNavigation } from "./icons/RightNavigation";
-
+export { default as Loading } from "./icons/Loading";
 export { loginIllustration, userImage };
