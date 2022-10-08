6549f714e190bc798388a07be8497c773e5cf011
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 16:24:42 +0100

    exported components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Input } from "./molecules/Input";
 export { default as PassWordInput } from "./molecules/PassWordInput";
-export { default as Loader } from "./molecules/Loader/Loader";
+export { default as Loader } from "./atoms/Loader/Loader";
 export { default as Layout } from "./molecules/Layout";
 export { default as Navbar } from "./molecules/Navbar";
 export { default as Sidebar } from "./molecules/Sidebar";
@@ -14,3 +14,4 @@ export { default as TableHeader } from "./atoms/TableHeader";
 export { default as SelectInput } from "./atoms/SelectInput";
 export { default as SelectDate } from "./atoms/SelectDate";
 export { default as UserDetailSummary } from "./molecules/UserDetailSummary";
+export { default as UserInfo } from "./molecules/UserInfo";
