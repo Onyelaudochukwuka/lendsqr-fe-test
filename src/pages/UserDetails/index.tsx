cec2f2f3d467df4a1d8c44c14e9427c779ec6ddf
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 08:11:19 +0100

    added details

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import { LeftArrow } from "../../assets";
+import { Layout } from "../../components";
 import style from "./index.module.css";
-interface IUserDetails {
-
-}
+interface IUserDetails {}
 const UserDetails: FC<IUserDetails> = () => {
-    return (
-        <div className={style.UserDetails}>
-            UserDetails
+  return (
+    <Layout className={style.UserDetails}>
+      <Link to={"/dashboard/users"}>
+        <div className={style.UserDetails__back}>
+          <LeftArrow className={style.UserDetails__back__icon} />{" "}
+          <span className={style.UserDetails__back__text}>Back to Users</span>
         </div>
-
-    );
-}
+      </Link>
+          <div className={style.UserDetails__container}>
+              <h2 className={style.UserDetails__container__heading}>User Details</h2>
+        <div className={style.UserDetails__container__buttons}>
+          <button className={`${style.UserDetails__container__buttons__button} ${style.UserDetails__container__buttons__button__blacklist}`}>Blacklist User</button>
+          <button className={`${style.UserDetails__container__buttons__button} ${style.UserDetails__container__buttons__button__activate}`}>Activate User</button>
+        </div>
+          </div>
+          
+    </Layout>
+  );
+};
 
 export default UserDetails;
