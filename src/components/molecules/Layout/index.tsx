49a51a0866df4205705fc00b0962f10596d61fc6
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 23:16:04 +0100

    mobile responsiveness fixes

--- src/components/molecules/Layout/index.tsx
+++ src/components/molecules/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useState } from "react";
 import { Navbar, Sidebar } from "../..";
 import style from "./index.module.css";
 interface ILayout {
@@ -6,11 +6,17 @@ interface ILayout {
   className: string;
 }
 const Layout: FC<ILayout> = ({ children, className, ...props }) => {
+  const [showSideBar, setShowSideBar] = useState(false);
   return (
     <section className={style.Layout}>
-      <Navbar className={style.Layout__nav} />
-      <Sidebar className={style.Layout__sidebar} />
-      <section {...props} className={`${className} ${style.Layout__body}`}>{children}</section>
+      <Navbar
+        className={style.Layout__nav}
+        {...{ showSideBar, setShowSideBar }}
+      />
+      <Sidebar className={`${style.Layout__sidebar} ${showSideBar && style.Layout__sidebar__active}`} />
+      <section {...props} className={`${className} ${style.Layout__body}`}>
+        {children}
+      </section>
     </section>
   );
 };
