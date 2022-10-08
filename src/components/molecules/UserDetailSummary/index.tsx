95204744070c43368349a9427bb7334bf72c3622
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 11:33:02 +0100

    addec logic for text

--- src/components/molecules/UserDetailSummary/index.tsx
+++ src/components/molecules/UserDetailSummary/index.tsx
@@ -1,15 +1,29 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
+import { avatar } from "../../../assets";
 import style from "./index.module.css";
-interface IUserDetailSummary {
-
-}
+interface IUserDetailSummary {}
 const UserDetailSummary: FC<IUserDetailSummary> = () => {
-    return (
-        <div className={style.UserDetailSummary}>
-            UserDetailSummary
-        </div>
+  return (
+    <div className={style.UserDetailSummary}>
+      <div className={style.UserDetailSummary__container}>
+              <div className={style.UserDetailSummary__container__left}>
+                  
+          <img
+            src={avatar}
+            alt="user"
+            className={style.UserDetailSummary__container__left__image}
+                  />
+                  <div
+                  className={style.UserDetailSummary__container__left__details}
+                  >
+                      <div className={style.UserDetailSummary__container__left__details__name}>Grace Effiom</div>
+                      <div className={style.UserDetailSummary__container__left__details__accountId}>LSQFf587g90</div>
 
-    );
-}
+                  </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UserDetailSummary;
