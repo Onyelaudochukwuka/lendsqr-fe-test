82b51eee4a7204e89c703bfc14c02cc3002c5f90
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 23:20:01 +0100

    exported pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export { default as Login } from "./LogIn";
 export { default as UserDashboard } from "./UserDashboard";
 export { default as UserDetails } from "./UserDetails";
+export { default as Unavailable } from "./Unavailable";
+
