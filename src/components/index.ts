88a326e5f78704e4f9cb416cc31b62d0c821e7fe
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 11:12:08 +0100

    exported user details

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,3 +13,4 @@ export { default as SelectViews } from "./atoms/SelectViews";
 export { default as TableHeader } from "./atoms/TableHeader";
 export { default as SelectInput } from "./atoms/SelectInput";
 export { default as SelectDate } from "./atoms/SelectDate";
+export { default as UserDetailSummary } from "./molecules/UserDetailSummary";
