93a0371b630c43c8eeaebaf55d56ba72f4707bed
Udoka <udochukwukaonyela@gmail.com>
2022-10-08 23:19:44 +0100

    created unavailable for page not yet built

---
+++ src/pages/Unavailable/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import { Layout } from "../../components";
+import style from "./index.module.css";
+interface IUnavailable {}
+const Unavailable: FC<IUnavailable> = () => {
+  return (
+    <Layout className={style.Unavailable}>
+      <div className={style.Unavailable__container}>
+        <h1 className={style.Unavailable__container__heading}>
+          This Page Is Currently Unavailable
+        </h1>
+        <p className={style.Unavailable__container__text}>
+          Check in Later
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Unavailable;
