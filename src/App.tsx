2f2ddee323845f240d613112807d085b4cd2d9b8
Udoka <udochukwukaonyela@gmail.com>
2022-10-09 07:41:32 +0100

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import React, { FC, Fragment, useEffect } from "react";
-import "./App.css";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import { Login, Unavailable, UserDashboard, UserDetails } from "./pages";
-const App: FC = () => {
-  return (
-    <Fragment>
-      <MoveToTop />
-      <Routes>
-        <Route path="/logIn" element={<Login />} />
-        <Route path="/dashboard" element={<Unavailable />} />
-        <Route path="/dashboard/:route" element={<Unavailable />} />
-        <Route path="/dashboard/users" element={<UserDashboard />} />
-        <Route path="/dashboard/users/:id" element={<UserDetails />} />
-      </Routes>
-    </Fragment>
-  );
-};
+import React, { FC, Fragment, useEffect } from 'react';
+import {
+  Route, Routes, useLocation, useNavigate,
+} from 'react-router-dom';
+
+import './App.css';
+import {
+  Login, Unavailable, UserDashboard, UserDetails,
+} from './pages';
+
 const MoveToTop = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { hash, pathname } = useLocation();
-  console.log(pathname)
   useEffect(() => {
-    if (pathname === "/") {
-      navigate("/logIn")
+    if (pathname === '/') {
+      navigate('/logIn');
     }
-    if (!!hash) return;
+    if (hash) return;
 
     window.scrollTo(0, 0);
   }, [pathname, navigate, hash]);
   return null;
 };
 
+const App: FC = () => (
+  <>
+    <MoveToTop />
+    <Routes>
+      <Route path="/logIn" element={<Login />} />
+      <Route path="/dashboard" element={<Unavailable />} />
+      <Route path="/dashboard/:route" element={<Unavailable />} />
+      <Route path="/dashboard/users" element={<UserDashboard />} />
+      <Route path="/dashboard/users/:id" element={<UserDetails />} />
+    </Routes>
+  </>
+);
 export default App;
