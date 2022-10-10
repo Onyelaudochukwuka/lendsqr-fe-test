da1c9c9ede21982de603cdb97fb2250fdf8de6f0
Udoka <udochukwukaonyela@gmail.com>
2022-10-10 07:46:08 +0100

    adding test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { render as rtlRender, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-test('renders learn react link', () => {
+const render = (ui: any, { route = '/logIn' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return rtlRender(ui, { wrapper: BrowserRouter });
+};
+
+test('full app rendering/navigating', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Login/i)).toBeInTheDocument();
+
+  userEvent.click(screen.getByText(/Login/i));
+
+  expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument();
+});
+
+test('landing on a bad page', () => {
+  render(<App />, { route: '/dashboard' });
+
+  expect(screen.getByText(/no match/i)).toBeInTheDocument();
 });
