5858ca45064af537be1094bc9f40c4c0a0d022f4
Udoka <udochukwukaonyela@gmail.com>
2022-10-10 12:52:19 +0100

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 
-import { render as rtlRender, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-
-const render = (ui: any, { route = '/logIn' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
-  return rtlRender(ui, { wrapper: BrowserRouter });
-};
+import { render, screen } from './testing-utils';
 
 test('full app rendering/navigating', () => {
   render(<App />);
-  expect(screen.getByText(/Login/i)).toBeInTheDocument();
+  expect(screen.getByText('Login')).toBeInTheDocument();
+  userEvent.click(screen.getByText('Login'));
 
-  userEvent.click(screen.getByText(/Login/i));
+  expect(screen.getByText('Please enter a password with an Uppercase, Lowercase, Number and a special character')).toBeInTheDocument();
+  expect(screen.getByText('Please enter a valid email')).toBeInTheDocument();
+  userEvent.type(screen.getByPlaceholderText('Email'), 'udochuks');
+  expect(screen.getByPlaceholderText('Email')).toHaveValue('udochuks');
+  userEvent.type(screen.getByPlaceholderText('Password'), 'udochuks');
+  expect(screen.getByPlaceholderText('Password')).toHaveValue('udochuks');
 
-  expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument();
+  userEvent.click(screen.getByText('Login'));
+
+  expect(screen.getByText('Please enter a password with an Uppercase, Lowercase, Number and a special character')).toBeInTheDocument();
+  expect(screen.getByText('Please enter a valid email')).toBeInTheDocument();
+
+  // clear inputs
+  userEvent.clear(screen.getByPlaceholderText('Email'));
+  userEvent.clear(screen.getByPlaceholderText('Password'));
+
+  userEvent.type(screen.getByPlaceholderText('Email'), 'udochuks@gmail.com');
+  expect(screen.getByPlaceholderText('Email')).toHaveValue('udochuks@gmail.com');
+  userEvent.type(screen.getByPlaceholderText('Password'), 'HDHD33hdhd3_');
+  expect(screen.getByPlaceholderText('Password')).toHaveValue('HDHD33hdhd3_');
+  userEvent.click(screen.getByText('Login'));
+  expect(screen.getByTestId('user-heading')).toBeInTheDocument();
 });
 
-test('landing on a bad page', () => {
+test('landing on the home page', () => {
   render(<App />, { route: '/dashboard' });
-
-  expect(screen.getByText(/no match/i)).toBeInTheDocument();
+  expect(screen.getByText('This Page Is Currently Unavailable')).toBeInTheDocument();
+});
+test('landing on a bad page', () => {
+  render(<App />, { route: '/dashboard/users' });
+  expect(screen.getByTestId('user-heading')).toBeInTheDocument();
 });
