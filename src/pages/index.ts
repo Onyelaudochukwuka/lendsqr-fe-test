4e48a5f5b407d8ed2f19a244a816376c8ec51e17
Udoka <udochukwukaonyela@gmail.com>
2022-10-10 12:58:34 +0100

    exported not found page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Login } from './LogIn';
 export { default as UserDashboard } from './UserDashboard';
 export { default as UserDetails } from './UserDetails';
 export { default as Unavailable } from './Unavailable';
+export { default as NotFound } from './NotFound';
