6c54b225400d49fd83b8f24c2ecf6d2c40758350
Udoka <udochukwukaonyela@gmail.com>
2022-10-10 13:24:34 +0100

    created notfound page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React, { FC } from 'react';
+import { Link } from "react-router-dom";
+import { Notfound } from "../../assets";
+import style from './index.module.css';
+interface INotFound { }
+const NotFound: FC<INotFound> = () => {
+    return <div className={style.NotFound}>
+        <Notfound className={style.NotFound__illustration} />
+        <p className={style.NotFound__details}>Opps! Page Not Found</p>
+        <Link to="/dashboard" className={style.NotFound__button}>Back To Home</Link>
+    </div>;
+};
+
+export default NotFound;
